d33d974e2369b94777b0d7df3fec43d2a5c51aa2
Cyanhead <bunmioye09@gmail.com>
2022-09-06 22:39:55 +0100

    set up global styles and theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/Global.style';
+import { theme } from './styles/theme';
 
 function App() {
   return (
     <div className="App">
-      <div>Nav bar</div>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <div>Nav bar</div>
 
-      <Router>
-        <Routes>
-          <Route path="/" element={<div>PLP</div>} />
-          <Route path="/product" element={<div>PDP</div>} />
-          <Route path="/cart" element={<div>Cart</div>} />
-        </Routes>
-      </Router>
+        <Router>
+          <Routes>
+            <Route path="/" element={<div>PLP</div>} />
+            <Route path="/product" element={<div>PDP</div>} />
+            <Route path="/cart" element={<div>Cart</div>} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+  color: {
+    primary: '#5ECE7B',
+  },
+};
