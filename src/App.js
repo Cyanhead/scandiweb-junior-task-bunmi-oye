7a67d53957490a4f73d48979ab7fe74e221b831d
Cyanhead <bunmioye09@gmail.com>
2022-09-06 22:43:06 +0100

    change App to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/Global.style';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 
-function App() {
-  return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <div>Nav bar</div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <div>Nav bar</div>
 
-        <Router>
-          <Routes>
-            <Route path="/" element={<div>PLP</div>} />
-            <Route path="/product" element={<div>PDP</div>} />
-            <Route path="/cart" element={<div>Cart</div>} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </div>
-  );
+          <Router>
+            <Routes>
+              <Route path="/" element={<div>PLP</div>} />
+              <Route path="/product" element={<div>PDP</div>} />
+              <Route path="/cart" element={<div>Cart</div>} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
