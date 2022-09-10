478a539644d17e8c723a6823274be955a270cf77
Cyanhead <bunmioye09@gmail.com>
2022-09-10 21:54:19 +0100

    add apollo provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
