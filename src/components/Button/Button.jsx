593a22a7937e51cc2d44689252be1b9477407f86
Cyanhead <bunmioye09@gmail.com>
2022-09-17 16:15:04 +0100

    add onClick props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,19 +4,34 @@ import { ButtonComponent, ButtonLink } from './button.style';
 
 export class Button extends Component {
   render() {
+    const {
+      bg,
+      bgHover,
+      bgActive,
+      fg,
+      pad,
+      border,
+      fontSize,
+      width,
+      height,
+      onClick,
+      children,
+    } = this.props;
+
     return (
       <ButtonComponent
-        bg={this.props.bg}
-        bgHover={this.props.bgHover}
-        bgActive={this.props.bgActive}
-        fg={this.props.fg}
-        pad={this.props.pad}
-        border={this.props.border}
-        fontSize={this.props.fontSize}
-        width={this.props.width}
-        height={this.props.height}
+        bg={bg}
+        bgHover={bgHover}
+        bgActive={bgActive}
+        fg={fg}
+        pad={pad}
+        border={border}
+        fontSize={fontSize}
+        width={width}
+        height={height}
+        onClick={onClick}
       >
-        {this.props.children}
+        {children}
       </ButtonComponent>
     );
   }
