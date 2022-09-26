6277e345c78d6b8fee596d7a03ca7c6b09e375cd
Bunmi Oye <bunmioye09@gmail.com>
2022-09-26 04:18:05 +0100

    create CartImage Component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -32,6 +32,59 @@ import { Button } from '../../components/Button';
 
 import arrow from '../../assets/images/arrow.svg';
 
+class CartImage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImage: 0,
+    };
+  }
+
+  setNextImage = length => {
+    if (this.state.currentImage === length - 1) {
+      this.setState({
+        currentImage: 0,
+      });
+    } else {
+      this.setState({
+        currentImage: this.state.currentImage + 1,
+      });
+    }
+  };
+
+  setPrevImage = length => {
+    if (this.state.currentImage === 0) {
+      this.setState({
+        currentImage: length - 1,
+      });
+    } else {
+      this.setState({
+        currentImage: this.state.currentImage - 1,
+      });
+    }
+  };
+
+  render() {
+    const { images } = this.props;
+    return (
+      <ImageWrap>
+        <Image src={images[this.state.currentImage]} alt="" />
+        {images.length > 1 && (
+          <Arrows>
+            <ArrowWrap onClick={() => this.setPrevImage(images.length)}>
+              <ArrowLeft src={arrow} alt="" />
+            </ArrowWrap>
+            <ArrowWrap onClick={() => this.setNextImage(images.length)}>
+              <ArrowRight src={arrow} alt="" />
+            </ArrowWrap>
+          </Arrows>
+        )}
+      </ImageWrap>
+    );
+  }
+}
+
 class Cart extends Component {
   render() {
     const { cartItems } = this.props;
@@ -86,17 +139,7 @@ class Cart extends Component {
                     </ProductLeft>
                     <ProductRight>
                       <Counter id={newId} quantity={quantity} />
-                      <ImageWrap>
-                        <Image src={gallery[0]} alt="" />
-                        <Arrows>
-                          <ArrowWrap>
-                            <ArrowLeft src={arrow} alt="" />
-                          </ArrowWrap>
-                          <ArrowWrap>
-                            <ArrowRight src={arrow} alt="" />
-                          </ArrowWrap>
-                        </Arrows>
-                      </ImageWrap>
+                      <CartImage images={gallery} />
                     </ProductRight>
                   </ProductRow>
                 );
