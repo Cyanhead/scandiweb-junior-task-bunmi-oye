29dea634431e7de1ef669377e5557e104f50c236
Bunmi Oye <bunmioye09@gmail.com>
2022-09-26 14:40:25 +0100

    add local storage for cart items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { createStore } from 'redux';
+import { loadState, saveState } from '../helper/localStorage';
 
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const initialState = loadState();
+
+const store = createStore(rootReducer, initialState);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
